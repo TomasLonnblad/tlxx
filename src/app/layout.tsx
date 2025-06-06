d1f95fa720681ec6bd25eb3bd3036414b196770e
Tomas <tomaslonnblad@gmail.com>
2025-06-06 18:42:12 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeSwitcher } from "./_components/theme-switcher";
 
 import "./globals.css";
 
-const inter = Inter({ subsets: ["englich"] });
+const inter = Inter({ subsets: ["english"] });
 
 export const metadata: Metadata = {
   title: `Next.js Blog Example with ${CMS_NAME}`,
